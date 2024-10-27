22207e59316585745f65c4d6b8f479d79c88ed93
Paulo Borges <borgespaulo72@yahoo.com.br>
2024-10-27 07:16:08 -0300

    feat: Correções no Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,6 @@ import styles from './Header.module.css'
 
 import igniteLogo from '../assets/ignite_simbol.svg'
 
-console.log(igniteLogo)
-
 const Header = () => {
   return (
     <header className={styles.header}>
